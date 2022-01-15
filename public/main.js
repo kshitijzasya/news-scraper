015a353a8bacf6c2c15397b0cf026266dfac8ae3
kshitij <kshtjsharma68@gmail.com>
2022-01-15 17:44:39 +0530

    style

--- public/main.js
+++ public/main.js
@@ -1,12 +1,15 @@
 const scrape = document.getElementById("scrape");
 const url = document.getElementById("url");
 const date = document.getElementById("date-limit");
+const spinner = document.getElementsByClassName('spinner-border')[0];
 
 scrape.addEventListener("click", async(e) => {
   e.preventDefault(); 
+  spinner.style.display = "block";
   const response = await fetch(`/scrape?url= ${url.value}&date=${date.value}`);
   //resolve a promise  
   if (response.status == 200 ) {
     window.open('/download', '_blank');
   }
+  spinner.style.display = "none";
 });
