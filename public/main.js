73d51ff77494d4635b5f4602fadf867a1b3d5ee1
kshitij <kshtjsharma68@gmail.com>
2022-01-17 11:32:20 +0530

    condition to proceed

--- public/main.js
+++ public/main.js
@@ -6,10 +6,16 @@ const spinner = document.getElementsByClassName('spinner-border')[0];
 scrape.addEventListener("click", async(e) => {
   e.preventDefault(); 
   spinner.style.display = "block";
-  const response = await fetch(`/scrape?url= ${url.value}&date=${date.value}`);
+  console.log(url.value, date.value)
+  if (url.value.length && date.value.length) {
+    const response = await fetch(`/scrape?url= ${url.value}&date=${date.value}`);
   //resolve a promise  
   if (response.status == 200 ) {
     window.open('/download', '_blank');
   }
+  } else {
+    window.alert('Need valid inputs to proceed')
+  }
+  
   spinner.style.display = "none";
 });
